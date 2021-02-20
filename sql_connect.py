e49ec9a42c12fbab7cdabf8a776da58725ead9b1
wdzxy7 <1192936732@qq.com>
2021-02-20 11:39:39 +0800

    加入初始化数据库环境文件

---
+++ sql_connect.py
@@ -0,0 +1,9 @@
+import pymysql
+from sqlalchemy import create_engine
+
+
+class sql_c:
+    def __init__(self):
+        self.engine = create_engine('mysql+pymysql://root:@localhost:3308/unknown_data', encoding='utf8')
+        connect = pymysql.connect(host='localhost', port=3308, user='root', passwd='', db='', charset='utf8')
+        self.cursor = connect.cursor()
